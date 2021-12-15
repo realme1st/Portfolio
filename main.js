6b6e0b3f09867baf984eec3a5fd7fa54113649bd
SeongMin Lee <62661142+realme1st@users.noreply.github.com>
2021-12-15 17:40:24 +0900

    Javascript arrow up

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ document.addEventListener('scroll',() =>{
     }else {
         navbar.classList.remove('navbar--dark');
     }
-})
+});
 
 // scrolling when clicking on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
@@ -21,13 +21,13 @@ navbarMenu.addEventListener('click',(event) => {
         return;
     }
     scrollIntoView(link);
-})
+});
 
 // click on "contact me" button on home
 const homeContactBtn = document.querySelector('.home__contact');
 homeContactBtn.addEventListener('click',() => {
     scrollIntoView('#contact');
-})
+});
     
 
 // Make home slowly fade to transparent as the window scrolls down 
@@ -36,8 +36,22 @@ const homeHeight = home.getBoundingClientRect().height;
 
 document.addEventListener('scroll',()=> {
     home.style.opacity=1-window.scrollY / homeHeight;
-})
+});
 
+// Show "arrow up" button when scrolling item
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll',() => {
+    if(window.scrollY > homeHeight/2){
+        arrowUp.classList.add('visible');
+    }else{
+        arrowUp.classList.remove('visible');
+    }
+});
+
+// Handle click on the "arrow up" button
+arrowUp.addEventListener('click',() => {
+    scrollIntoView('#home');
+})
 
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
